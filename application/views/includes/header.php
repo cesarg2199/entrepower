1a632d2a5015464ca3e7e76663f34bac933f22f6
Cesar Guerrero Jr <cesarguerrero@Cesars-MacBook.local>
2016-04-10 22:25:31 -0400

    Began the Entrepower website and started the design for the homepage

---
+++ application/views/includes/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>EntrePOWER</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css');?>">
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/entrepower.css');?>">
+</head>
+<body>
+<nav class="navbar navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">EntrePOWER</a>
+      <!-- Maybe do logo, not sure <a class="navbar-brand" href="#"><img src="<?php echo base_url('assets/img/logo.jpg');?>" alt="EntrePOWER"></a> -->
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#">Home</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">EntrePOWER<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">POWER Training</a></li>
+          <li><a href="#">Entre Ciriculum</a></li>
+        </ul>
+      </li>
+      <li><a href="#">About Us</a></li>
+      <li><a href="#">Contact Us</a></li>
+      <li><a href="#">Alumni</a></li>
+      <li><a href="#">Job Employment</a></li>
+    </ul>
+  </div>
+</nav>
+  
+<div class="container">
